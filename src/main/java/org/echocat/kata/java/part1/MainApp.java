f8a774f6aef60ee5e57666fc2a21eac35341ea41
Akos Kitta <kittaakos@typefox.io>
2021-04-05 14:17:52 +0200

    use Gradle
    Signed-off-by: Akos Kitta <kittaakos@typefox.io>

--- src/main/java/org/echocat/kata/java/part1/MainApp.java
+++ src/main/java/org/echocat/kata/java/part1/MainApp.java
@@ -1,9 +1,16 @@
 package org.echocat.kata.java.part1;
-
+import org.eclipse.swt.widgets.*;
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println(getHelloWorldText());
+        Display display = new Display ();
+        Shell shell = new Shell(display);
+        shell.setText("Snippet 1");
+        shell.open ();
+        while (!shell.isDisposed ()) {
+            if (!display.readAndDispatch ()) display.sleep ();
+        }
+        display.dispose ();
     }
 
     protected static String getHelloWorldText() {
